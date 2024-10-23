eafcd74bc271071d2a52951d6dddd17f63565f58
Arthur Abade <arthurnz.personal@gmail.com>
2024-10-23 18:38:37 -0300

    formatacao

--- sistema-modelo/sistema_modelo/src/main/java/com/pi4jsm/model/entities/Produto.java
+++ sistema-modelo/sistema_modelo/src/main/java/com/pi4jsm/model/entities/Produto.java
@@ -12,45 +12,43 @@ public class Produto {
     private Integer quantidade; 
     private ValidadeStatus statusValidade; 
 
-    private Produto (Integer id, String nome, LocalDate dataDeValidade, Integer quantidade){
+    public Produto (Integer id, String nome, LocalDate dataDeValidade, Integer quantidade) {
         this.id = id;
         this.nome = nome;
         this.dataDeValidade = dataDeValidade;
         this.quantidade = quantidade; 
     }
+    public Produto() {
 
+    }
 
-    public Integer getId(){
+    public Integer getId() {
         return id;
     }
-
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public LocalDate getDataDeValidade(){
+    public LocalDate getDataDeValidade() {
         return dataDeValidade;
     }
-    public void setDataDeValidade(LocalDate dataDeValidade){
+    public void setDataDeValidade(LocalDate dataDeValidade) {
         this.dataDeValidade = dataDeValidade;
     }
-
-    public Integer getQuantidade(){
+    public Integer getQuantidade() {
         return quantidade;
     }
-    public void setQuantidade(Integer quantidade){
+    public void setQuantidade(Integer quantidade) {
         this.quantidade = quantidade;
     }
-
-    public ValidadeStatus getStatusValidade(){
+    public ValidadeStatus getStatusValidade() {
         return statusValidade;
     }
 
     // Método 
-    public void verificarValidade(){
+    public void verificarValidade() {
         double dias = Duration.between(dataDeValidade, LocalDate.now()).toDays();
         
         if(dias > 15){
@@ -64,5 +62,6 @@ public class Produto {
                 this.statusValidade = ValidadeStatus.VENCIDO;
             }
         }
+        
     }
 }
