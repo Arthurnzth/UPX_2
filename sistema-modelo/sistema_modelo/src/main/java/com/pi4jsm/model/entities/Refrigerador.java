1767a081c98f64f1bd0adb0c1be2f990e4d9c140
Arthur Abade <arthurnz.personal@gmail.com>
2024-10-23 20:56:22 -0300

    Add metodos quantidade total & atributos

--- sistema-modelo/sistema_modelo/src/main/java/com/pi4jsm/model/entities/Refrigerador.java
+++ sistema-modelo/sistema_modelo/src/main/java/com/pi4jsm/model/entities/Refrigerador.java
@@ -1,5 +1,8 @@
 package com.pi4jsm.model.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
@@ -13,9 +16,11 @@ public class Refrigerador {
     private final GpioController gpio;
     private final GpioPinDigitalInput sensorPorta;
     private final GpioPinDigitalInput sensorQuimico;
-    private final GpioPinDigitalOutput luzPorta;
+    private final GpioPinDigitalOutput luzSensorPorta;
     private final GpioPinDigitalOutput compressor;
     private final GpioPinDigitalOutput luzOutputQuimico;
+
+    private List<Produto> produtos = new ArrayList<>();
 
     public Refrigerador(){
         // Cria uma instância do GpioFactory para controlar os pinos
@@ -24,9 +29,61 @@ public class Refrigerador {
         // Inicializa e define os estados dos atributos, além de configurar em cada pino
         this.sensorPorta = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, "Sensor da Porta");
         this.sensorQuimico = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04, "Sensor quimico");
-        this.luzPorta = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "Luz da Porta", PinState.LOW);
+        this.luzSensorPorta = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "Luz do Sensor da Porta", PinState.LOW);
         this.compressor = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "Compressor", PinState.LOW);
         this.luzOutputQuimico = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Luz do Sensor Quimico", PinState.LOW);
 
     }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    // Método para saber a quantidade total em estoque
+    public int quantidadeTotalEstoque() {
+        int total = 0;
+        for (Produto p : produtos) {
+            total += p.getQuantidade();
+        }
+        return total;
+    }
+
+    // Método para saber a quantidade total de um produto x
+    public int quantidadeTotalProdutoX(Produto produto) {
+        int total = 0;
+        for (Produto p : produtos) {
+            if (produtos.stream().filter(x->x.getId()==produto.getId()).findFirst().orElse(null) != null) {
+                total += p.getQuantidade();
+            }
+        }
+        return total;
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
