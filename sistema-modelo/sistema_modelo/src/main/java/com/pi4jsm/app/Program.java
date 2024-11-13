5ecb7c942df887ab1d626980a074309ddb38b15d
Arthur Abade <arthurnz.personal@gmail.com>
2024-11-12 21:44:11 -0300

    classe principal aplicacao

--- sistema-modelo/sistema_modelo/src/main/java/com/pi4jsm/app/Program.java
+++ sistema-modelo/sistema_modelo/src/main/java/com/pi4jsm/app/Program.java
@@ -1,7 +1,28 @@
 package com.pi4jsm.app;
 
-public class Program {
-    
+import java.io.IOException;
 
+import com.pi4jsm.model.entities.Refrigerador;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Refrigerador refrigerador = new Refrigerador();
+
+        try {
+            refrigerador.loop();
+        }
+        catch (InterruptedException e) {
+
+        }
+        catch (IOException e) {
+
+        }
+        finally {
+            refrigerador.finalizar();
+        }
+        
+    }
 
 }
