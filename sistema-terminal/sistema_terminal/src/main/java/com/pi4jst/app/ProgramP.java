bd6fc5734e0f6f2d7ab89463c3218c5241740db6
Arthur Abade <arthurnz.personal@gmail.com>
2024-11-26 13:29:54 -0300

    classe main app P

---
+++ sistema-terminal/sistema_terminal/src/main/java/com/pi4jst/app/ProgramP.java
@@ -0,0 +1,17 @@
+package com.pi4jst.app;
+
+import java.util.Scanner;
+
+public class ProgramP {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+        
+    }
+
+}
